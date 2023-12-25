0f7da12f83016fa35f403e04006102233bc2bc7f
abdalluhfayez420@gmail.com <77704981+fayez666@users.noreply.github.com>
2023-12-25 02:35:26 +0200

    Counter App with BloC

---
+++ lib/inc_dec_page.dart
@@ -0,0 +1,36 @@
+import 'package:counter_app/bloc/counter_bloc.dart';
+import 'package:counter_app/cubit/counter_cubit.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class IncDecPage extends StatelessWidget {
+  const IncDecPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final counterCubit = BlocProvider.of<CounterCubit>(context);
+    final counterBloc = BlocProvider.of<CounterBloc>(context);
+
+    return Scaffold(
+      floatingActionButton: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          FloatingActionButton(
+            onPressed: () {
+              counterBloc.add(CounterIncremented());
+            },
+            tooltip: 'Increment',
+            child: const Icon(Icons.add),
+          ),
+          FloatingActionButton(
+            onPressed: () {
+              counterBloc.add(CounterDecremented());
+            },
+            tooltip: 'Decrement',
+            child: const Icon(Icons.minimize),
+          ),
+        ],
+      ),
+    );
+  }
+}
